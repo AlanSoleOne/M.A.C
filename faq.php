75f02e36e2f8e59cf0036b0320a807d74a3a3746
AlanSoleOne <alan.dsole@gmail.com>
2018-08-31 22:30:09 -0300

    preguntasa_base

--- faq.php
+++ faq.php
@@ -1,5 +1,29 @@
 <?php
-
-
-
+//Título----------------------------------------------------
+echo "<h2>Preguntas frecuentes</h2>";
+echo "<br>";
+echo "<br>";
+//Opcion 1--------------------------------------------------
+echo "<ul><li><h4>Quiénes somos?</h4></li></ul>";
+//Respuesta--------------------------
+echo "<i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolor.</i>";
+echo "<br>";
+echo "<br>";
+//Opcion 2--------------------------------------------------
+echo "<ul><li><h4>Cuáles son los medios de pago?</li></ul></h4>";
+//Respuesta--------------------------
+echo "<i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illo quia
+ rerum doloribus consectetur provident pariatur rem ut nesciunt quis.</i>";
+echo "<br>";
+echo "<br>";
+//Opcion 3--------------------------------------------------
+echo "<ul><li><h4>Dónde retiro mis 'Sticks'?</li></ul></h4>";
+//Respuesta--------------------------
+echo "<i>Lorem ipsum dolor sit amet consectetur.</i>";
+echo "<br>";
+echo "<br>";
+//Opcion 4--------------------------------------------------
+echo "<ul><li><h4>Tienen local físico?</h4></li></ul>";
+//Respuesta--------------------------
+echo "<i>Lorem ipsum dolor sit amet consectetur adipisicing.</i>";
 ?>
