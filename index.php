10d3ab66761ede8e63623aab1840054dfaa85570
MaruGargiulo <marugargiulo@gmail.com>
2018-09-02 23:22:07 -0300

    home de Stickear

--- index.php
+++ index.php
@@ -5,9 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700" rel="stylesheet">
+    <link rel="icon" href="favicon.png" type="image/x-icon" />
+    <title>Stickear. Tu universo de figuritas</title>
 </head>
 <body>
+
+    <div class="container"></div> <!-- INICIO CONTENEDOR GENERAL -->
+        
+        <header>
+            <img class="logo" src="img/logo.png" alt="">
+            <img class="logo-mobile" src="img/logo-mobile.png" alt="">
+        </header>
+
+        <nav class="inicio-sesion">
+            <a href="">Iniciar sesión</a>
+            <a href="">REGISTRARSE</a>
+        </nav>
+
+        <main>
+            <div class="main-slogan">
+                <p class="titulo">Tu universo de figuritas</p>
+                <p class="slogan">Encontrá las que te faltan, <br>vendé las que tenés.</p>
+            </div>
+        </main>
+
+        
+    </div> <!-- FIN CONTENEDOR GENERAL -->
     
 </body>
 </html>
