34d12393bc907daf7e4e452c967988df57ec7e12
MaruGargiulo <marugargiulo@gmail.com>
2018-09-04 18:28:38 -0300

    final(por ahora)

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">    
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/estyle.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700" rel="stylesheet">
     <link rel="icon" href="favicon.png" type="image/x-icon" />
     <title>Stickear. Tu universo de figuritas</title>
@@ -19,11 +20,14 @@
             <img class="logo" src="img/logo.png" alt="">
             <img class="logo-mobile" src="img/logo-mobile.png" alt="">
         </header>
+            <nav class="inicio-sesion">
+                <a href="faq.php">Preguntas frecuentes</a>
+                <a class="boton-inicio" href="usuario.html">Iniciar sesión</a>
+                <a href="formulario.php">REGISTRARSE</a>
+            </nav>
 
-        <nav class="inicio-sesion">
-            <a href="">Iniciar sesión</a>
-            <a href="">REGISTRARSE</a>
-        </nav>
+
+        
 
         <main>
             <div class="main-slogan">
@@ -31,6 +35,10 @@
                 <p class="slogan">Encontrá las que te faltan, <br>vendé las que tenés.</p>
             </div>
         </main>
+
+        <footer>
+            
+        </footer>
 
         
     </div> <!-- FIN CONTENEDOR GENERAL -->
