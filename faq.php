805ed6b362ca894587cfb84ce4c04631c4e09b28
AlanSoleOne <alan.dsole@gmail.com>
2018-09-03 22:05:40 -0300

    faq_css

--- faq.php
+++ faq.php
@@ -1,29 +1,72 @@
-<?php
-//Título----------------------------------------------------
-echo "<h2>Preguntas frecuentes</h2>";
-echo "<br>";
-echo "<br>";
-//Opcion 1--------------------------------------------------
-echo "<ul><li><h4>Quiénes somos?</h4></li></ul>";
-//Respuesta--------------------------
-echo "<i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolor.</i>";
-echo "<br>";
-echo "<br>";
-//Opcion 2--------------------------------------------------
-echo "<ul><li><h4>Cuáles son los medios de pago?</li></ul></h4>";
-//Respuesta--------------------------
-echo "<i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illo quia
- rerum doloribus consectetur provident pariatur rem ut nesciunt quis.</i>";
-echo "<br>";
-echo "<br>";
-//Opcion 3--------------------------------------------------
-echo "<ul><li><h4>Dónde retiro mis 'Sticks'?</li></ul></h4>";
-//Respuesta--------------------------
-echo "<i>Lorem ipsum dolor sit amet consectetur.</i>";
-echo "<br>";
-echo "<br>";
-//Opcion 4--------------------------------------------------
-echo "<ul><li><h4>Tienen local físico?</h4></li></ul>";
-//Respuesta--------------------------
-echo "<i>Lorem ipsum dolor sit amet consectetur adipisicing.</i>";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Preguntas frecuentes</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700" rel="stylesheet">
+    <link rel="icon" href="favicon.png" type="image/x-icon" />
+</head>
+<body>
+    <div>
+<!--Título-->
+<h2>Preguntas frecuentes</h2>
+    </div>
+<br>
+<br>
+<!--Opcion 1-->
+<ul>
+        <li>
+            <h4>Quiénes somos?</h4>
+        </li>
+      </ul>
+<!--Respuesta-->
+<i>Somos 3 estudiantes de <a href="https://www.digitalhouse.com/curso/programacion-web-fullstack/">"Programación Web Full Stack"</a>  en el Turno Noche del 
+instituto <a href="https://www.digitalhouse.com/campus/belgrano/">Digital House - Belgrano</a> y esta es la primera parte del proyecto.</i>
+<br>
+<br>
+<!--Opcion 2-->
+<ul>
+        <li>
+            <h4>Cuáles son los medios de pago?</h4>
+        </li>
+</ul>
+<!--Respuesta-->
+            <ol>
+                <h3>Los medios de pago disponibles son:</h3>
+            
+                <li>Tarjetas de crédito <b>(Visa, Master, Cabal).</b></li>
+                <li>MercadoPago</li>
+                <li>PagoMisCuentas(Te enviaremos un mail con el botón de pago)</li>
+                <li>Ualá</li>
+            </ol>
+<br>
+<br>
+<!--Opcion 3-->
+<ul>
+        <li>
+            <h4>Dónde retiro mis 'Sticks'?</h4>
+        </li>
+</ul>
+<!--Respuesta-->
+<i>Podes acordar con el vendedor, o bien elegir el envío a domicilio.</i>
+<br>
+<br>
+<!--Opcion 4-->
+<ul>
+        <li>
+            <h4>Tienen local físico?</h4>
+        </li>
+</ul>
+<!--Respuesta-->
+<i>No poseemos local, ya que le brindamos la posibilidad de vender a quienes tengan
+<strong>'sticks'</strong> repetidas. Y a quienes compran, le ofrecemos la libertad de
+elegir el método de envío o retiro en un punto de encuentro. </i>
+</body>
+</html>
+
+
+
