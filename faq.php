f4b84e53935ece3b40905bd1004541b507247dbd
AlanSoleOne <alan.dsole@gmail.com>
2018-09-19 22:10:18 -0300

    va_quedando

--- faq.php
+++ faq.php
@@ -11,67 +11,78 @@
     <link rel="icon" href="favicon.png" type="image/x-icon" />
 </head>
 <body>
-    <div class="container">
-
+    
         <header>
-            <a href="index.html">
+        <a href="index.html">
             <img class="logo" src="img/logo.png" alt="">
             </a>
             
             <img class="logo-mobile" src="img/logo-mobile.png" alt="">
-        </header>
+
+          </header>
+    <div class="container">
+            
+       
 <!--Título-->
 <h1>Preguntas frecuentes</h1>
 <br>
 <br>
 <!--Opcion 1-->
-<ul>
+
         <li>
             <h4>Quiénes somos?</h4>
         </li>
-      </ul>
+<br>
+      
 <!--Respuesta-->
 <i>Somos 3 estudiantes de <a target="_blank" href="https://www.digitalhouse.com/curso/programacion-web-fullstack/">"Programación Web Full Stack"</a>  en el Turno Noche del 
 instituto <a target="_blank" href="https://www.digitalhouse.com/campus/belgrano/">Digital House - Belgrano</a> y esta es la primera parte del proyecto.</i>
 <hr>
 <br>
 <!--Opcion 2-->
-<ul>
+
         <li>
             <h4>Cuáles son los medios de pago?</h4>
         </li>
-</ul>
+<br>
+
 <!--Respuesta-->
-            <ol>
-                <h3>Los medios de pago disponibles son:</h3>
             
+                <h3>Los medios de pago disponibles son:</h3>
+            <br>
                 <li>Tarjetas de crédito <b>(Visa, Master, Cabal).</b></li>
+            <br>
                 <li>MercadoPago</li>
+            <br>
                 <li>PagoMisCuentas(Te enviaremos un mail con el botón de pago)</li>
+            <br>
                 <li>Ualá</li>
-            </ol>
+            
 <hr>
 <br>
 <!--Opcion 3-->
-<ul>
+
         <li>
             <h4>Dónde retiro mis 'Sticks'?</h4>
         </li>
-</ul>
+<br>
+
 <!--Respuesta-->
 <i>Podes acordar con el vendedor, o bien elegir el envío a domicilio.</i>
 <hr>
 <br>
 <!--Opcion 4-->
-<ul>
+
         <li>
             <h4>Tienen local físico?</h4>
         </li>
-</ul>
+<br>
+
 <!--Respuesta-->
 <i>No poseemos local, ya que le brindamos la posibilidad de vender a quienes tengan
 <strong>'sticks'</strong> repetidas. Y a quienes compran, le ofrecemos la libertad de
 elegir el método de envío o retiro en un punto de encuentro. </i>
+
     </div>
 </body>
 </html>
